dc577c31efca1f87910df73aacf4035427101276
Coda Hale <coda.hale@gmail.com>
2024-02-12 13:16:45 -0700

    perf: switch to criterion

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,34 +1,34 @@
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use rand_chacha::ChaChaRng;
 use rand_core::SeedableRng;
 
-#[divan::bench]
-fn key_gen(bencher: divan::Bencher) {
-    bencher.with_inputs(|| ChaChaRng::seed_from_u64(0xDEADBEEF)).bench_values(mlkem768::key_gen)
+fn key_gen(c: &mut Criterion) {
+    c.bench_function("key-gen", |b| {
+        let rng = ChaChaRng::seed_from_u64(0xDEADBEEF);
+        b.iter_batched(|| rng.clone(), mlkem768::key_gen, BatchSize::SmallInput);
+    });
 }
 
-#[divan::bench]
-fn encapsulate(bencher: divan::Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut rng = ChaChaRng::seed_from_u64(0xDEADBEEF);
-            let (ek, _) = mlkem768::key_gen(&mut rng);
-            (rng, ek)
-        })
-        .bench_values(|(mut rng, ek)| mlkem768::encapsulate(&ek, &mut rng))
+fn encapsulate(c: &mut Criterion) {
+    c.bench_function("encapsulate", |b| {
+        let mut rng = ChaChaRng::seed_from_u64(0xDEADBEEF);
+        let (ek, _) = mlkem768::key_gen(&mut rng);
+        b.iter_batched(
+            || rng.clone(),
+            |rng| mlkem768::encapsulate(&ek, rng),
+            BatchSize::SmallInput,
+        );
+    });
 }
 
-#[divan::bench]
-fn decapsulate(bencher: divan::Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut rng = ChaChaRng::seed_from_u64(0xDEADBEEF);
-            let (ek, dk) = mlkem768::key_gen(&mut rng);
-            let (c, _) = mlkem768::encapsulate(&ek, &mut rng).expect("should encapsulate");
-            (dk, c)
-        })
-        .bench_refs(|(dk, c)| mlkem768::decapsulate(dk, c))
+fn decapsulate(c: &mut Criterion) {
+    c.bench_function("decapsulate", |b| {
+        let mut rng = ChaChaRng::seed_from_u64(0xDEADBEEF);
+        let (ek, dk) = mlkem768::key_gen(&mut rng);
+        let (c, _) = mlkem768::encapsulate(&ek, &mut rng).expect("should encapsulate");
+        b.iter(|| mlkem768::decapsulate(&dk, &c).expect("should decapsulate"));
+    });
 }
 
-fn main() {
-    divan::main()
-}
+criterion_group!(benches, key_gen, encapsulate, decapsulate);
+criterion_main!(benches);
